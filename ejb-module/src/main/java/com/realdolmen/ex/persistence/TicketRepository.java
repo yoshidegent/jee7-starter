f757ab52ecfff6900b6acbd4f529ffc58010a60d
YDEAX41 <yoshi_degent@hotmail.com>
2015-09-11 14:34:57 +0200

    small error fix

--- ejb-module/src/main/java/com/realdolmen/ex/persistence/TicketRepository.java
+++ ejb-module/src/main/java/com/realdolmen/ex/persistence/TicketRepository.java
@@ -6,4 +6,7 @@ import com.realdolmen.ex.domain.Ticket;
  * Created by YDEAX41 on 10/09/2015.
  */
 public class TicketRepository extends GenericRepository<Ticket> {
+    public TicketRepository(Class<Ticket> persistentClass) {
+        super(persistentClass);
+    }
 }
