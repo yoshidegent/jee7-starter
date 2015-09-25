95abeeaf496f185f436e39a186e51f7dd4d73f1a
YDEAX41 <yoshi_degent@hotmail.com>
2015-09-25 18:41:16 +0200

    staged some new files

--- ejb-module/src/test/java/com/realdolmen/ex/persistence/PassengerRepositoryCrudTest.java
+++ ejb-module/src/test/java/com/realdolmen/ex/persistence/PassengerRepositoryCrudTest.java
@@ -17,7 +17,6 @@ import java.util.Date;
  */
 public class PassengerRepositoryCrudTest extends DataSetPersistenceTest
 {
-
     private PassengerRepository passengerRepository;
     private Passenger passenger;
 
